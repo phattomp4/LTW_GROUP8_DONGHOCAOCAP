c3a44e9f37c11e30ac18a2d1ecfd213a6b2bc74c
TÔ TẤN PHÁT <23130232@st.hcmuaf.edu.vn>
2025-11-06 06:21:52 +0700

    up

--- scirptIndex.js
+++ scirptIndex.js
@@ -28,6 +28,38 @@ function showSlides(n) {
     dots[slideIndex-1].className += " active";
 }
 
+// SwiperJS
+new Swiper('.card-wrapper', {
+    loop: true,
+    spaceBetween: 30,
+
+    // Pagination bullets
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // Responsive breakpoints
+    breakpoint: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    }
+});
+
 
 /* phan script cua Vu */
         const page2 = document.getElementById('Page2');
