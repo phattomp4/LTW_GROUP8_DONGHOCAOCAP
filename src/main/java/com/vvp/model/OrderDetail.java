3da6c6ece8e660a9d8bbf4794f979b8ded4ab02b
NguyenTruongVu <23130389@st.hcmuaf.edu.vn>
2026-01-22 21:58:04 +0700

    Tạo OrderDAO, Order, OrderDetail
    Dùng để lưu đơn hàng sau khi thanh toán

---
+++ src/main/java/com/vvp/model/OrderDetail.java
@@ -0,0 +1,49 @@
+package com.vvp.model;
+
+public class OrderDetail {
+    private int orderDetailId;
+    private int orderId;
+    private int productId;
+    private int quantity;
+    private double priceAtPurchase;
+
+    public int getOrderDetailId() {
+        return orderDetailId;
+    }
+
+    public void setOrderDetailId(int orderDetailId) {
+        this.orderDetailId = orderDetailId;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPriceAtPurchase() {
+        return priceAtPurchase;
+    }
+
+    public void setPriceAtPurchase(double priceAtPurchase) {
+        this.priceAtPurchase = priceAtPurchase;
+    }
+}
